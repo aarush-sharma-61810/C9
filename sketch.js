5f6d2ce2c8a513f3a7370d79371ca420159261b3
aarush-sharma-61810 <91355916+aarush-sharma-61810@users.noreply.github.com>
2021-10-24 09:49:33 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var ball
+
+
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(200,200,10,10);
+  ball.shapeColor = "blue"
+  
+}
+
+function draw() 
+{
+  background(30);
+
+  drawSprites()
+}
+
+
+
+
